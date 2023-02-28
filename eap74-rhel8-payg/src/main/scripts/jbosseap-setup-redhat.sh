7e0a9fa94d95622c2be6641eefb1e679f20ec733
Zheng Chang <zhengchang@microsoft.com>
2023-02-28 15:07:32 +0800

    Add walk around to cert issues
    Signed-off-by: Zheng Chang <zhengchang@microsoft.com>

--- eap74-rhel8-payg/src/main/scripts/jbosseap-setup-redhat.sh
+++ eap74-rhel8-payg/src/main/scripts/jbosseap-setup-redhat.sh
@@ -87,8 +87,13 @@ fi
 ####################### 
 
 # Install JBoss EAP 7.4
-echo "subscription-manager repos --enable=jb-eap-7.4-for-rhel-8-x86_64-rpms" | log; flag=${PIPESTATUS[0]}
-subscription-manager repos --enable=jb-eap-7.4-for-rhel-8-x86_64-rpms | log; flag=${PIPESTATUS[0]}
+# echo "subscription-manager repos --enable=jb-eap-7.4-for-rhel-8-x86_64-rpms" | log; flag=${PIPESTATUS[0]}
+# subscription-manager repos --enable=jb-eap-7.4-for-rhel-8-x86_64-rpms | log; flag=${PIPESTATUS[0]}
+
+# Install JBoss EAP 7.4
+echo "Enable Microsoft repos" | log; flag=${PIPESTATUS[0]}
+echo "yum update -y --disablerepo='*' --enablerepo='*microsoft*'" | log; flag=${PIPESTATUS[0]}
+yum update -y --disablerepo='*' --enablerepo='*microsoft*' | log; flag=${PIPESTATUS[0]}
 if [ $flag != 0 ] ; then echo  "ERROR! Enabling repos for JBoss EAP Failed" >&2 log; exit $flag;  fi
 
 echo "Installing JBoss EAP 7.4 repos" | log; flag=${PIPESTATUS[0]}
